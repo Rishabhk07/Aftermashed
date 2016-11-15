50da7c123253f808dd549fefc1e699ef4fb33c46
Rishabh Khanna <rishabhkhanna777@gmail.com>
2016-11-15 19:13:44 +0530

    index page done

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const Parse = require('Parse/node');
 const path = require('path');
 
 
+app.use('/' , express.static(__dirname + "/public"));
 
 app.use('/',(req, res)=> {
     Parse.initialize("ucfS6neahiGB0BOd1aAfV7HxQTye5K0U4r40N1O3", "4igpUls0v3KRQI2o4dhNx8uTWUduMcyUuxQqsYSH");
     Parse.serverURL = "https://parseapi.back4app.com/";
+    res.sendFile(__dirname + "/public/html/index.html");
 });
 
 
