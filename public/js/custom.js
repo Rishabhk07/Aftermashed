9249beda0986ec70221876ffdd37fc97edb2f508
Rishabh Khanna <rishabhkhanna777@gmail.com>
2016-11-16 01:08:55 +0530

    login added

---
+++ public/js/custom.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rishabhkhanna on 15/11/16.
+ */
+
+$(function () {
+    $('.mash').click(function () {
+        console.log("Hello");
+        $('.disappear').hide(50);
+
+    })
+});
+
--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const app = express();
 const Parse = require('Parse/node');
 const path = require('path');
 
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+        clientID: 1320253381341248,
+        clientSecret: "9ca8ba88d2bdeba99320d6985b05a2cd" ,
+        callbackURL: "http://localhost:333/"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        if(profile != null){
+            console.log(profile.name);
+        }
+    }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/',
+        failureRedirect: '/login' }));
 
 app.use('/' , express.static(__dirname + "/public"));
 
